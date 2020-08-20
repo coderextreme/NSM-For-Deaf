f4316e30418711286e4dafceddfb21a017a209d3
John Carlson <yottzumm@gmail.com>
2020-08-20 02:17:00 -0500

    added progress added instructions for viewing html

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # NSM-For-Deaf
 Natural Semantic Metalanguage for Deaf
+
+* Run `sh make.sh`
+
+* Start a web server
+
+python -m http.server
+
+* go to localhost
+
+http://locahost:8000/
+http://locahost:8000/interface.html
--- make.sh
+++ make.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # rm -r frames mp4s primes videos.txt html_primes html_nsm
+echo mkdr
 mkdir -p frames mp4s primes html_primes html_nsm
+echo deaf.pl
 perl deaf.pl < EnglishNSM.txt | sh | sort -u | sh
+echo nsm.pl
 perl nsm.pl < EnglishNSM.txt > interface.html
+echo deaf2.pl
 perl deaf2.pl < Eng2.txt | sh | sort -u | sh
+echo nsm2.pl
 perl nsm2.pl < Eng2.txt > index.html
+echo getglyphogram.py
 cat primes.txt | tr ' ' '\0' | xargs -0 python getglyphogram.py
+echo goddard.py
 cat primesandphrases.txt | tr '\n' '\0' | xargs -0 python goddard.py
